7eb1c6c49dd4b5d86062161a9c99d31d1659d03f
Frankrike <lisa.vallfors@gmail.com>
2017-05-03 15:00:46 +0200

    Changed functions to classes and added putUser method

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,33 +1,52 @@
 // Holds all data
-function DataManager() {
-    this.chats = {};
-    this.users = {};
-}
-// Add a chat, if it doesn't already exist.
-DataManager.prototype.putChat = function(chatObject) {
-    if (chatObject.id in this.chats)
-        return;
-    this.chats[chatObject.id] = new ChatData(chatObject);
-}
-// Add a message to the chat with id chatId
-DataManager.prototype.putMessage = function(chatId, messageObject) {
-    this.chats[chatId].putMessage(messageObject);
+class DataManager {
+    constructor() {
+        this.chats = {};
+        this.users = {};
+    }
+
+    // Add a chat, if it doesn't already exist.
+
+    putChat(chatObject) {
+        if (chatObject.id in this.chats)
+            return;
+        this.chats[chatObject.id] = new ChatData(chatObject);
+    }
+
+    // Add a user, if it doesn't already exist.
+    putUser(userObject) {
+        if (userObject.id in this.users)
+            return;
+        this.users[userObject.id] = userObject;
+    }
+
+    // Add a message to the chat with id chatId
+    putMessage(chatId, messageObject) {
+        this.chats[chatId].putMessage(messageObject);
+    }
+
 }
 
 
-// Holds data for a specific Chat
-function ChatData(chatObject) {
-    Object.assign(this, chatObject); //Merge the data from the object (title, id etc) into this one
-    
-    this.messages = []; //List of message objects
-}
-// Add a message to the chat
-ChatData.prototype.putMessage = function(messageObject){
-    this.messages.push(new MessageData(messageObject));
+class ChatData {
+    constructor(chatObject) {
+        Object.assign(this, chatObject); //Merge the data from the object (title, id etc) into this one
+        this.messages = []; //List of message objects
+    }
+
+    putMessage(messageObject) {
+        this.messages.push(new MessageData(messageObject));
+    }
+
 }
 
-// Holds data for a specific message.
-function MessageData(messageObject) {
-    Object.assign(this, messageObject);
-    self.timeSent = new Date();
+class MessageData {
+    constructor(messageObject) {
+        Object.assign(this, messageObject);
+        self.timeSent = new Date();
+    }
+
 }
+
+
+
