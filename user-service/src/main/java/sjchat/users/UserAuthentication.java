b66f75c6b63cdff40b070a4cf5c8957b6d9d1615
Jonval <jonval@kth.se>
2017-05-06 23:00:18 +0200

    Fixed synthactical Errors

--- user-service/src/main/java/sjchat/users/UserAuthentication.java
+++ user-service/src/main/java/sjchat/users/UserAuthentication.java
@@ -2,28 +2,29 @@ package sjchat.users;
 
 import io.jsonwebtoken.*;
 import java.util.Date;
-import sjchat.users.SJJwtAuthentication;
-import sjchat.users.SJJwtBuilder;
 import sjchat.users.AuthenticationResult;
 import sjchat.restapi.entities.User;
+import sjchat.users.TokenAuth;
+import sjchat.users.TokenBuilder;
 public class UserAuthentication {
     
-    private SJJwtAuthentication JWSauth;
-    private SJJwtBuilder JWSbuilder;
+    private TokenAuth auth;
+    private TokenBuilder builder;
 
 
     public UserAuthentication(String issuer, byte[] secret) {
-        this.auth = new SJJwtAuthentication(issuer, secret);
-        this.builder = new SJJwtBuilder(issuer, secret);
+        this.auth = new TokenAuth(issuer, secret);
+        this.builder = new TokenBuilder(issuer, secret);
+        
     }
 
     String tokenize(User user, Date expiration) {
-        return this.JWSbuilder.build(user.getUsername(), expiration);
+        return this.builder.build(user.getUsername(), expiration);
     }
 
     AuthenticationResult authenticate(User user, String jws) {
         try {
-           return new AuthenticationResult(this.JWSauth.authenticate(user.getUsername(), jws), true);
+           return new AuthenticationResult(this.auth.authenticate(user.getUsername(), jws), true);
         } catch (Exception e) {
             return new AuthenticationResult(e.getMessage(), false);
         }
