7a5f62fe32d323035aa63e2733a2bfbedee0693b
Jesper Fridefors <jesper.fridefors@gmail.com>
2017-05-09 10:41:45 +0200

    fixed whitespace and indentation

--- user-service/src/main/java/sjchat/users/tokens/AuthenticationResult.java
+++ user-service/src/main/java/sjchat/users/tokens/AuthenticationResult.java
@@ -3,14 +3,14 @@ package sjchat.users.tokens;
 import java.util.Date;
 
 public class AuthenticationResult {
-    
-    public final String message;
-    public final boolean result;
-    public final Date issuedAt;
 
-    public AuthenticationResult(String message, boolean result, Date issuedAt) {
-        this.message = message;
-        this.result = result;
-        this.issuedAt = issuedAt;
-    }
+  public final String message;
+  public final boolean result;
+  public final Date issuedAt;
+
+  public AuthenticationResult(String message, boolean result, Date issuedAt) {
+    this.message = message;
+    this.result = result;
+    this.issuedAt = issuedAt;
+  }
 }
