df7c1eff97d0b6ebb739d1e5c0d4859ba2ac0573
Johan Vikström <tritzium@gmail.com>
2017-05-09 11:53:53 +0200

    Made it possible to create chats

--- database-service/src/main/java/sjchat/entities/ChatEntity.java
+++ database-service/src/main/java/sjchat/entities/ChatEntity.java
@@ -1,6 +1,7 @@
 package sjchat.entities;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +17,38 @@ public class ChatEntity {
     private String title;
     @Column(name="participants")
     private List<String> participantIds;
+
+    public ChatEntity(){}
+    public ChatEntity(String id, String title, List<String> participantIds){
+        this.id = id;
+        this.title = title;
+        this.participantIds = participantIds;
+    }
+
+    public static class Builder{
+        private String id;
+        private String title;
+        private List<String> participantIds = new ArrayList<String>();
+        public Builder setId(String id){
+            this.id = id;
+            return this;
+        }
+        public Builder setTitle(String title){
+            this.title = title;
+            return this;
+        }
+        public Builder addParticipant(String id){
+            participantIds.add(id);
+            return this;
+        }
+        public Builder setParticipants(List<String> ids){
+            participantIds = ids;
+            return this;
+        }
+        public ChatEntity build(){
+            return new ChatEntity(id, title, participantIds);
+        }
+    }
 
     public String getId(){
         return id;
