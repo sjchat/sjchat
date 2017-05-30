4da2299126706d89ef597a67640084f10c3ff7e0
Jonval <jonval@kth.se>
2017-05-30 21:20:20 +0200

    Removed Duplicate code in UserEntity to fix build

--- database-service/src/main/java/sjchat/entities/UserEntity.java
+++ database-service/src/main/java/sjchat/entities/UserEntity.java
@@ -50,10 +50,6 @@ public class UserEntity {
     this.username = username;
   }
 
-  public String getPassword() {
-    return password;
-  }
-
   public void setPassword(String password) {
     this.password = password;
   }
